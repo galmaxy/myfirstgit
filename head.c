f779e04496fb70b6767dc4fee07070a7140cdabf
galmaxy <galmaxy@gmail.com>
2017-12-13 21:11:46 +0800

    try to push orgin default

--- head.c
+++ head.c
@@ -11,5 +11,9 @@ This change from local branch.
 
 4 version;
 head.c only edit under master.
+5 version;
 如何
 如何使用vim和git来编辑一个文本文件，进行写作。记录创作的过程。这应该是一件还有意思的事情。
+
+6 version;
+now try to push with 默认origin.
