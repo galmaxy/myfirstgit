20add335dbeb373ea907dc57814622ee281d8bb7
galmaxy <galmaxy@gmail.com>
2017-12-19 17:06:41 +0800

    修改head.c, 对以前的数据进行修改

--- head.c
+++ head.c
@@ -7,13 +7,20 @@ This chang form local reprository.
 This chang form github.
 
 3 version;
-This change from local branch.
+这一行是进行修改的。
 
 4 version;
 head.c only edit under master.
+  这里是在7 version的时候添加到内容。
+  
 5 version;
 如何
 如何使用vim和git来编辑一个文本文件，进行写作。记录创作的过程。这应该是一件还有意思的事情。
 
 6 version;
 now try to push with 默认origin.
+
+7 version;
+对以前的数据进行修改。
+然后把3version的一行放到了最后。
+This change from local branch.
